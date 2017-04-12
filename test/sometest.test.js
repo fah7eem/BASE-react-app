21c8bcde7a259849c18accc6fea37db7e7c6b41c
fah7eem <fseedat7@gmail.com>
2017-04-12 16:19:21 +0200

    Moved test setups.js and test mocha

--- test/sometest.test.js
+++ test/sometest.test.js
@@ -1,7 +1,10 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import { expect } from 'chai'
-import App from '../src/App'
+import React, {component} from 'react';
+import { shallow} from 'enzyme';
+import { expect } from 'chai';
+import {describe, it} from 'mocha';
+import {spy} from 'sinon';
+import App from '../src/App';
+
 
 const wrapper = shallow(<App />);
 
@@ -9,4 +12,13 @@ describe('(Component) App', () => {
   it('renders...', () => {
     expect(wrapper).to.have.length(1);
   });
+});
+
+describe('<ContactRelationshipForm/>', () => {
+    context("when first rendered", () => {
+        it("contains <App/> contains class App", () => {
+            //noinspection JSUnresolvedVariable
+            expect(wrapper.find('.App')).to.have.length(1);
+        });
+    });
 });
